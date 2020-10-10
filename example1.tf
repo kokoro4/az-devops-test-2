d68d79102dba82759e2be4bd41899202198642c1
Michael Casinha <kokoro4@gmail.com>
2020-10-09 20:26:38 -0700

    removing storage account creation

--- example1.tf
+++ example1.tf
@@ -20,10 +20,10 @@ resource "azurerm_resource_group" "tamopsrg" {
   location = "westus2"
 }
 
-resource "azurerm_storage_account" "tamopssa" {
-  name                     = "tamopssatf"
-  resource_group_name      = azurerm_resource_group.tamopsrg.name
-  location                 = azurerm_resource_group.tamopsrg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
+# resource "azurerm_storage_account" "tamopssa" {
+#   name                     = "tamopssatf"
+#   resource_group_name      = azurerm_resource_group.tamopsrg.name
+#   location                 = azurerm_resource_group.tamopsrg.location
+#   account_tier             = "Standard"
+#   account_replication_type = "LRS"
+# }
