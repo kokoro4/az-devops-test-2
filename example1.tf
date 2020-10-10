c1ad858b2d35db62e694e01f793fe90dbaa5e937
Michael Casinha <kokoro4@gmail.com>
2020-10-09 19:31:18 -0700

    Adding TF code

---
+++ example1.tf
@@ -0,0 +1,29 @@
+provider "azurerm" {
+    # The "feature" block is required for AzureRM provider 2.x.
+    # If you're using version 1.x, the "features" block is not allowed.
+    version = "~>2.0"
+    features {}
+}
+
+terraform {
+    backend "azurerm" {
+        resource_group_name = "z1p-cont-1"    
+        storage_account_name = "azuswtfz1pstate01"
+        container_name = "azdevopstest2"
+    }
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "tamopsrg" {
+  name     = "tamops-tf"
+  location = "westus2"
+}
+
+resource "azurerm_storage_account" "tamopssa" {
+  name                     = "tamopssatf"
+  resource_group_name      = azurerm_resource_group.tamopsrg.name
+  location                 = azurerm_resource_group.tamopsrg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
