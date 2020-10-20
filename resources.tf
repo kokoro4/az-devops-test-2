e908c39d9ae1fe6b21cb2eebd70f341e371b34a0
kokoro4 <kokoro4@gmail.com>
2020-10-19 19:28:47 -0700

    removing storage account

--- resources.tf
+++ resources.tf
@@ -3,10 +3,10 @@ resource "azurerm_resource_group" "tamopsrg" {
   location = "westus2"                                                                                                                                                                             
 }                                                                                                                                                                                                  
                                                                                                                                                                                                    
-resource "azurerm_storage_account" "tamopssa" {                                                                                                                                                    
-  name                     = "tamopssatf1"                                                                                                                                                         
-  resource_group_name      = azurerm_resource_group.tamopsrg.name                                                                                                                                  
-  location                 = azurerm_resource_group.tamopsrg.location                                                                                                                              
-  account_tier             = "Standard"                                                                                                                                                            
-  account_replication_type = "LRS"                                                                                                                                                                 
-}          
+#resource "azurerm_storage_account" "tamopssa" {                                                                                                                                                    
+#  name                     = "tamopssatf1"                                                                                                                                                         
+#  resource_group_name      = azurerm_resource_group.tamopsrg.name                                                                                                                                  
+#  location                 = azurerm_resource_group.tamopsrg.location                                                                                                                              
+#  account_tier             = "Standard"                                                                                                                                                            
+#  account_replication_type = "LRS"                                                                                                                                                                 
+#}          
